f48376c594a75a5d89cad77f9947a1eb6c23394a
Adriano Porto <adriano.porto.a.j@gmail.com>
2024-03-10 13:33:51 -0300

    format: project formatting

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,10 @@
-import logoSvg from "../../assets/Logo.svg";
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-export function Header () {
+import logoSvg from '../../assets/Logo.svg'
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+export function Header() {
     return (
         <HeaderContainer>
             <HeaderContent>
-                <img src={logoSvg} alt="" />    
+                <img src={logoSvg} alt="" />
                 <NewTransactionButton>Nova Transação</NewTransactionButton>
             </HeaderContent>
         </HeaderContainer>
